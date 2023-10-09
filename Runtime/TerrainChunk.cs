10871c3e74cde0ebeb07b772dcc03845d9f53400
Terry Lockett <terryjlockett@gmail.com>
2023-10-09 15:12:21 -0300

    Forgot to press save in code :|

--- Runtime/TerrainChunk.cs
+++ Runtime/TerrainChunk.cs
@@ -46,15 +46,11 @@ namespace McTerrain {
 
 		//TODO JFC fix this duplicate code
 		public void makeBombHole(Vector3 location, float radius) {
-			Debug.Log("transform.position");
-
-
 			bool isTerrainDirty = false;
 			for (int x = 0; x < CHUNK_SIZE + 1; x++) {
 				for (int y = 0; y < terrainData.getMapHeight(); y++) {
 					for (int z = 0; z < CHUNK_SIZE + 1; z++) {
 						VertexNode vNode = vertexNodes[x, y, z];
-
 						if (((vNode.getLocation()+ transform.position) - location).magnitude > radius) {
 							continue;
 						}
